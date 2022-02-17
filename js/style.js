364965fd06d3d43cdffe0a5e74fe85ef68a2a8cc
mukesh <mukeshsalaria01@gmail.com>
2022-02-17 19:21:43 +0530

    changes in login page

--- js/style.js
+++ js/style.js
@@ -1,30 +1,23 @@
-$(function() {  
+$(function () {
     // function on email-input feild
     $(".email-input").on({
-        focus:function(){  
-            $( this ).siblings(".basic-addon").addClass("primary-border");
+        focus: function () {
+            $(this).siblings(".basic-addon").addClass("primary-border");
         },
-        blur:function(){ 
-            $( this ).siblings(".basic-addon").removeClass("primary-border");
+        blur: function () {
+            $(this).siblings(".basic-addon").removeClass("primary-border");
         },
-        keypress:function(){ 
+        keypress: function () {
             $(".password-input").addClass("background-primary");
         }
-      });
-      $(".list-able").on({
-          click: function(){
-              $(this).removeClass("opacity-25").addClass("opacity-100");
-              $(this).children(".check-tick").toggleClass("d-none");
-              $(this).siblings().children(".check-tick").addClass("d-none");
-              $(this).siblings().addClass("opacity-25").removeClass("opacity-100");
-             
-          }
-      })
-      $(".lang-icon").on({
-          click: function(){
-              $(this).children(".fa-caret-lef").Class("d-none");
-             
-             
-          }
-      })
     });
+    //   function on dropdown menu
+    $(".list-able").on({
+        click: function () {
+            $(this).removeClass("opacity-50").addClass("opacity-100");
+            $(this).children(".check-tick").toggleClass("d-none");
+            $(this).siblings().children(".check-tick").addClass("d-none");
+            $(this).siblings().addClass("opacity-50").removeClass("opacity-100");
+        }
+    });
+});
